3f4151e86f3f243b72e604098ca68c451e8d7d6e
Fortie40 <elijahmurimi40@gmail.com>
2019-07-15 05:45:52 +0300

    Initial Commit

---
+++ app/src/main/java/com/fortie40/alc40/AboutAlc.java
@@ -0,0 +1,71 @@
+package com.fortie40.alc40;
+
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class AboutAlc extends AppCompatActivity {
+
+    private SwipeRefreshLayout mSwipe;
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_alc);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mSwipe = findViewById(R.id.swipe);
+        mSwipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadWeb();
+            }
+        });
+
+        loadWeb();
+    }
+
+
+    // Load the website to the web view
+    private void loadWeb() {
+        String url = "https://andela.com/alc/";
+
+        mWebView = findViewById(R.id.web_view);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl(url);
+        mSwipe.setRefreshing(true);
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                mWebView.loadUrl("file:///android_asset/error.html");
+                super.onReceivedError(view, request, error);
+            }
+
+            // Ignore SSL certificate
+            // Todo:  Implement certificate pinning
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                //super.onReceivedSslError(view, handler, error);
+                handler.proceed();
+            }
+
+            // Remove loader
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                mSwipe.setRefreshing(false);
+                super.onPageFinished(view, url);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/fortie40/alc40/MainActivity.java
@@ -0,0 +1,59 @@
+package com.fortie40.alc40;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        CardView about_alc = findViewById(R.id.about_alc);
+        CardView my_profile =findViewById(R.id.my_profile);
+
+        about_alc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AboutAlc.class));
+            }
+        });
+
+        my_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MyProfile.class));
+            }
+        });
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+}
